48db0e15c438e05a176a19a9676227edde54d050
chasepowers59 <cvpowers0903@gmail.com>
2025-12-02 07:02:18 -0700

    deploy3

--- knexfile.js
+++ knexfile.js
@@ -21,10 +21,10 @@ module.exports = {
     production: {
         client: 'pg',
         connection: {
-            host: process.env.RDS_HOSTNAME,
-            user: process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            database: process.env.RDS_DB_NAME,
+            host: process.env.RDS_HOSTNAME || process.env.DB_HOSTNAME,
+            user: process.env.RDS_USERNAME || process.env.DB_USERNAME,
+            password: process.env.RDS_PASSWORD || process.env.DB_PASSWORD,
+            database: process.env.RDS_DB_NAME || process.env.DB_NAME,
             port: process.env.RDS_PORT || 5432,
             ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
         },
