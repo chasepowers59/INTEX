9be1d3a11a123184831de1a14869d9f1ce0b24ee
chasepowers59 <cvpowers0903@gmail.com>
2025-12-02 08:50:37 -0700

    Add SQL import seed and disable initial data seed

---
+++ seeds/02_run_sql.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+    const sqlPath = path.join(__dirname, 'ella_rises_complete_FIXED.sql');
+    const sql = fs.readFileSync(sqlPath, 'utf8');
+
+    // Split by semicolon to run statements individually if needed, 
+    // but knex.raw usually handles multiple statements if the DB driver supports it.
+    // PostgreSQL driver supports multiple statements in one query.
+    await knex.raw(sql);
+};
